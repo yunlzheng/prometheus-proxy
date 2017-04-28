286af82b70d23d1ff49b180f08018378d30a2490
yunl.zheng <yunl.zheng@wise2c.com>
2017-04-28 14:51:52 +0800

    clean code

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"strings"
 
@@ -102,23 +101,6 @@ func instancesEndpoint(c *gin.Context) {
 		c.String(500, err.Error())
 	}
 	c.JSON(200, services)
-}
-
-func ReverseProxy() gin.HandlerFunc {
-	target := "localhost:3000"
-	return func(c *gin.Context) {
-		director := func(req *http.Request) {
-			r := c.Request
-			req = r
-			req.URL.Scheme = "http"
-			req.URL.Host = target
-			req.Header["my-header"] = []string{r.Header.Get("my-header")}
-			// Golang camelcases headers
-			delete(req.Header, "My-Header")
-		}
-		proxy := &httputil.ReverseProxy{Director: director}
-		proxy.ServeHTTP(c.Writer, c.Request)
-	}
 }
 
 func proxyEndpoint(c *gin.Context) {
